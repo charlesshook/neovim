9adc8558499fb4f7f7a07f8d5f51835d6f582eb0
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-05-08 18:32:10 -0500

    More lsp updates

--- lua/chuckalicious/plugins/lsp.lua
+++ lua/chuckalicious/plugins/lsp.lua
@@ -4,9 +4,16 @@ return {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
     },
 
     config = function()
+        local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
@@ -14,31 +21,33 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.default_capabilities())
 
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            },
-        })
-
+        require("fidget").setup({})
+        require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "pyright",
-                "gopls",
-                "svelte",
                 "tailwindcss",
+                "tsserver",
+                "svelte",
             },
             handlers = {
                 function(server_name)
+
                     require("lspconfig")[server_name].setup {
                         capabilities = capabilities
                     }
                 end,
-
             }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, 
+                { name = 'buffer' },
+            })
         })
 
         vim.diagnostic.config({
