dd23e2bff206e00b2640680a97ee70956398c14f
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-05-09 15:46:08 -0500

    Created keymaps to enable and disable copilot

--- lua/chuckalicious/keymaps.lua
+++ lua/chuckalicious/keymaps.lua
@@ -1,1 +1,18 @@
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Enable and Disable Github Copilot
+vim.keymap.set("n", "<leader>ce",
+    function()
+        vim.cmd("Copilot enable")
+        print("Copilot enabled")
+    end
+)
+
+vim.keymap.set("n", "<leader>cd",
+    function()
+        vim.cmd("Copilot disable")
+        print("Copilot disabled")
+    end
+)
