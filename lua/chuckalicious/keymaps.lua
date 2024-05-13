c5d8f0cdcb72da76edc4675e54b479916f06b5e4
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-05-13 15:59:16 -0600

    Added a autocmd to make the custom terminal window look better

--- lua/chuckalicious/keymaps.lua
+++ lua/chuckalicious/keymaps.lua
@@ -19,7 +19,18 @@ vim.keymap.set("n", "<leader>cd",
 
 
 -- Opens a new terminal at the bottom of the screen
-vim.keymap.set("n", "<leader>st", 
+local set = vim.opt_local
+
+vim.api.nvim_create_autocmd("TermOpen", {
+    group = vim.api.nvim_create_augroup("custom-term-open", {}),
+    callback = function()
+        set.number = false
+        set.relativenumber = false
+        set.scrolloff = 0
+    end,
+})
+
+vim.keymap.set("n", "<leader>st",
     function()
         vim.cmd.new()
         vim.cmd.wincmd "J"
