e1cb4655291e72a705cbf0a901e3e0a4babe3d55
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2025-03-07 15:23:56 -0700

    Setup now managed by .dotfile configuration. Updated key maps for telescope to switch files.

--- lua/chuckalicious/plugins/telescope.lua
+++ lua/chuckalicious/plugins/telescope.lua
@@ -3,5 +3,10 @@ return {
     tag = "0.1.6",
     dependencies = {
         "nvim-lua/plenary.nvim"
-    }
+    },
+    config = function()
+        require("telescope").setup {
+        }
+        vim.keymap.set("n", "<leader>fd", require('telescope.builtin').find_files)
+    end
 }
